3fb0ff769d9e21c9ccf93b3cbaf2401a83ead2df
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2022-03-02 19:22:47 +0500

    Wip rewrite

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-def get_fanfic(linkfan):
+query = "https://ficbook.net/find?fandom_filter=originals&fandom_group_id=1&pages_range=1&pages_min=&pages_max=&transl=1&likes_min=&likes_max=&rewards_min=&date_create_min=2022-01-05&date_create_max=2022-02-05&date_update_min=2022-01-05&date_update_max=2022-02-05&title=&sort=1&rnd=152877722&find=Найти%21&p={}"
+
+
+def find_links(seed):
     driver = uc.Chrome()
     with driver:
-        # передаём драйверу строку 
-        driver.get(linkfan)
+        # open the page with more links
+        driver.get(seed)
     with driver:
+        # find link tags
         elements = driver.find_elements_by_class_name("visit-link")
-        with open("links.txt", "w") as file:
-            for element in elements:
-                file.write(element.get_attribute('href') + "\n") 
-        # with open("fanfics/" + linkfan.split("/")[-1] + ".txt", "w") as file:
-            # file.write(element.get_attribute('innerText'))
+        # extract links from tags
+        links = [i.get_attribute('href') for i in elements]
     driver.close()
     driver.quit()
+    return links
 
-def get_fanfic(linkfan):
-    filepath = "fanfics/" + linkfan.split("/")[-1] + ".txt"
+def get_fanfic_text(link):
+    filepath = "fanfics/" + link.split("/")[-1] + ".txt"
     if isfile(filepath):
         return
     driver = uc.Chrome()
     with driver:
-        # передаём драйверу строку 
-        driver.get(linkfan)
+        # open the page with text
+        driver.get(link)
     try:
         with driver:
             element = driver.find_element(by="id", value="content")
-            with open(filepath, "w") as file:
-                file.write(element.get_attribute('innerText'))
+            text = element.get_attribute('innerText')
         driver.close()
         driver.quit()
+        return text
     except NoSuchElementException:
-        print(linkfan, "none")
+        # page contains no text
+        print(link, "no text found")
+        return ""
 
 def named_entity_recognition(a):
     doc = nlp(a)
     named_entities = []
     for ent in doc.ents:
-        named_entities.append(ent.text,
-              # ent.label_
-              )        
+        named_entities.append(ent.text)        
     return named_entities
-    # ner_list = nlp.pipe_labels['ner']
-    # for ner_category in ner_list:
-        # print("{} ({})".format(ner_category, spacy.explain(ner_category)))
 
+def scrape_for_fanfics(pagecount=5):
+    links = [get_links(query.format(page)) for page in range(1, pagecount)]
+    links = filter(None, links)
     
+        
