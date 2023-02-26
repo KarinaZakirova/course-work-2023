8bae157bf89d9c77c5ef3619211a517fb4a1b3dc
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-02-26 18:32:15 +0500

    Optimised loading, better logs

--- crawl.py
+++ crawl.py
@@ -15,7 +15,6 @@ class Crawler:
         self.driver = None
         self.query = "https://ficbook.net/find?fandom_filter=originals&fandom_group_id=1&pages_range=1&pages_min=&pages_max=&ratings%5B%5D=5&ratings%5B%5D=6&transl=1&likes_min=&likes_max=&rewards_min=&date_create_min=2023-02-25&date_create_max=2023-02-25&date_update_min=2023-02-25&date_update_max=2023-02-25&title=&sort=1&rnd=679840539&find=%D0%9D%D0%B0%D0%B9%D1%82%D0%B8%21&p={}"
         self.links = []
-        self.texts = []
 
     def __enter__(self):
         """
@@ -24,8 +23,6 @@ class Crawler:
         logger.debug(f"start chrome")
         self.driver = uc.Chrome()
         logger.debug(f"started chrome")
-        self.driver.get("https://www.google.com")
-        logger.debug(f"opened dummy page")
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -33,24 +30,21 @@ class Crawler:
         When finished or crashed, quit driver.
         """
         self.driver.quit()
-        print(len(self.links))
-        print(len(self.texts))
 
     def add_links(self, links):
         # Skip links to fanfics that have already been scraped.
         new_links = [link for link in links if not isfile(get_file_path(link))]
         self.links.extend(new_links)
-        logger.debug(f"added {len(new_links)} links, total count: {len(self.links)}")
+        logger.debug(f"learned {len(new_links)} links (total: {len(self.links)})")
 
-    def add_text(self, text):
+    def add_text(self, link, text):
         """
         New text has been received. Count the text and save it on the disk.
         """
-        self.texts.append(text)
         with open(get_file_path(link), "w", encoding="utf-8") as f:
             f.write(text)
 
-        logger.debug(f"added text, total count: {len(self.texts)}")
+        logger.debug(f"added text (total: {len(listdir('fanfics/'))})")
 
     def get_new_seed(self):
         """
@@ -61,7 +55,7 @@ class Crawler:
             yield self.query.format(page)
 
     def run(self, pagecount=2):
-        logger.debug(f"started running")
+        logger.debug(f"start running")
         while True:
             if not self.links:
                 # Ran out of fanfic links.
@@ -72,41 +66,40 @@ class Crawler:
             logger.debug(f"{link}")
             self.get_text_or_links(link)
 
-    def get_links(self, seed):
-        logger.debug(f"opening page for\t{seed}")
+    def load(self, link):
+        if self.driver.current_url == link:
+            logger.debug(f"same page, skip reloading - {link}")
+            return
+        logger.debug(f"loading - {link}")
         with self.driver:
-            # open the page with more links
-            self.driver.get(seed)
-        logger.debug(f"opened page for\t{seed}")
+            self.driver.get(link)
+        logger.debug(f"loaded - {link}")
+
+    def get_links(self, seed):
+        self.load(seed)
         with self.driver:
             # find link tags
             elements = self.driver.find_elements(By.CLASS_NAME, "visit-link")
             # extract links from tags
             links = [i.get_attribute('href') for i in elements]
             links = [link for link in links if link]
-        logger.debug(f"found elements for\t{seed}")
+        logger.debug(f"found links - {seed}")
         # self.driver.close()
-
         self.add_links(links)
 
     def get_text_or_links(self, link):
-        logger.debug(f"started chrome for\t{link}")
-        with self.driver:
-            # open the page with text
-            self.driver.get(link)
-        logger.debug(f"opened page for\t{link}")
+        self.load(link)
         try:
             with self.driver:
                 element = self.driver.find_element(by="id", value="content")
                 text = element.get_attribute('innerText')
             # self.driver.close()
-            logger.debug(f"found text, quit for\t{link}")
-
-            self.add_text(text)
+            logger.debug(f"found text - {link}")
+            self.add_text(link, text)
 
         except NoSuchElementException:
             # page contains no text
-            logger.debug(f"no text found, links for \t{link}")
+            logger.debug(f"no text, trying links - {link}")
             self.get_links(link)
 
 def get_file_path(link):
@@ -122,7 +115,8 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
 # create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(message)s')
 
 # add formatter to ch
 ch.setFormatter(formatter)
