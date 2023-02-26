5cc7830618d5fb49dfb7e9b7a998806acfa9f528
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-02-26 22:27:24 +0500

    Implement threading

--- crawl.py
+++ crawl.py
@@ -12,33 +12,90 @@ import re
 
 import logging
 
+from threading import Thread
+
 
 class Crawler:
-    def __init__(self):
-        self.driver = None
-        self.query = "https://ficbook.net/find?fandom_filter=originals&fandom_group_id=1&pages_range=1&pages_min=&pages_max=&ratings%5B%5D=5&transl=1&likes_min=&likes_max=&rewards_min=&date_create_min=2023-02-26&date_create_max=2023-02-26&date_update_min=2023-02-26&date_update_max=2023-02-26&title=&sort=1&rnd=1296979882&find=%D0%9D%D0%B0%D0%B9%D1%82%D0%B8%21&p={}"
+    def __init__(self, worker_count, text_limit):
+        self.workers = [Worker(self, index) for index in range(worker_count)]
+        self.logger = new_logger("crawler")
+        self.text_limit = text_limit
         self.links = []
+        self.tried_links = set()
+        self.query = "https://ficbook.net/find?fandom_filter=originals&fandom_group_id=1&pages_range=1&pages_min=&pages_max=&ratings%5B%5D=5&transl=1&tags_include%5B%5D=1669&likes_min=&likes_max=&rewards_min=&date_create_min=2023-02-26&date_create_max=2023-02-26&date_update_min=2023-02-26&date_update_max=2023-02-26&title=&sort=1&rnd=789441346&find=%D0%9D%D0%B0%D0%B9%D1%82%D0%B8%21&p={}"
+        self.query_page = 0
 
-    def __enter__(self):
+    def get_new_seed(self):
         """
-        Currently, no setup involved.
+        Yields a new link to a search page containing a list of fanfics.
         """
-        logger.debug(f"start chrome")
+        self.query_page += 1
+        self.logger.debug(f"get new seed #{self.query_page}")
+        return self.query.format(self.query_page)
+
+    def run(self):
+        self.logger.debug(f"start running")
+        for index, worker in enumerate(self.workers):
+            Thread(target = self.run_job_queue, args=(worker,), name=f"W_{index}").start()
+
+    def run_job_queue(self, worker):
+        worker.start()
+        while len(listdir('fanfics/')) < self.text_limit:
+            if not self.links:
+                # Ran out of fanfic links.
+                # Need to get new ones from Search page.
+                worker.get_links(self.get_new_seed())
+                continue
+            worker.get_text_or_links(self.get_link())
+
+    def get_link(self):
+        link = self.links.pop()
+        self.tried_links.add(link)
+        return link
+
+
+class Worker:
+    def __init__(self, crawler, index):
+        self.crawler = crawler
+        self.driver = None
+        self.logger = new_logger(f"worker{index}")
+
+    def start(self):
+        self.logger.debug(f"start chrome")
         self.driver = uc.Chrome()
-        logger.debug(f"started chrome")
-        return self
+        self.logger.debug(f"started chrome")
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        """
-        When finished or crashed, quit driver.
-        """
-        self.driver.quit()
+    def load(self, link):
+        if self.driver.current_url == link:
+            self.logger.debug(f"same page, skip reloading - {link}")
+            return
+        self.logger.debug(f"loading - {link}")
+        with self.driver:
+            self.driver.get(link)
+            # element = WebDriverWait(self.driver, 10).until(
+            #     EC.presence_of_element_located((By.TAG_NAME, "article"))
+            # )
+        self.logger.debug(f"loaded - {link}")
 
     def add_links(self, links):
-        # Skip links to fanfics that have already been scraped.
-        new_links = [link for link in links if not isfile(get_file_path(link))]
-        self.links.extend(new_links)
-        logger.debug(f"learned {len(new_links)} links (total: {len(self.links)})")
+        """
+        Skip links to fanfics that have already been scraped.
+        """
+        count = 0
+        for link in links:
+            if isfile(get_file_path(link)):
+                # Already downloaded.
+                continue
+            if link in self.crawler.links:
+                # Already queued.
+                continue
+            if link in self.crawler.tried_links:
+                # Already crawled before.
+                # (Might seem irrelevant, but likely helpful for fanfics with pages in them.)
+                continue
+            self.crawler.links.append(link)
+            count += 1
+        self.logger.debug(f"learned {count} links (total: {len(self.crawler.links)})")
 
     def add_text(self, link, text):
         """
@@ -47,39 +104,7 @@ class Crawler:
         with open(get_file_path(link), "w", encoding="utf-8") as f:
             f.write(text)
 
-        logger.debug(f"added text (total: {len(listdir('fanfics/'))})")
-
-    def get_new_seed(self):
-        """
-        Yields a new link to a search page containing a list of fanfics.
-        """
-        for page in range(1, 100):
-            logger.debug(f"get new seed #{page}")
-            yield self.query.format(page)
-
-    def run(self, pagecount=2):
-        logger.debug(f"start running")
-        while True:
-            if not self.links:
-                # Ran out of fanfic links.
-                # Need to get new ones from Search page.
-                self.get_links(next(self.get_new_seed()))
-                continue
-            link = self.links.pop()
-            logger.debug(f"{link}")
-            self.get_text_or_links(link)
-
-    def load(self, link):
-        if self.driver.current_url == link:
-            logger.debug(f"same page, skip reloading - {link}")
-            return
-        logger.debug(f"loading - {link}")
-        with self.driver:
-            self.driver.get(link)
-            # element = WebDriverWait(self.driver, 10).until(
-            #     EC.presence_of_element_located((By.TAG_NAME, "article"))
-            # )
-        logger.debug(f"loaded - {link}")
+        self.logger.debug(f"added text (total: {len(listdir('fanfics/'))})")
 
     def get_links(self, seed):
         self.load(seed)
@@ -89,7 +114,7 @@ class Crawler:
             # extract links from tags
             links = [i.get_attribute('href') for i in elements]
             links = [link for link in links if link]
-        logger.debug(f"found links - {seed}")
+        self.logger.debug(f"found links - {seed}")
         # self.driver.close()
         self.add_links(links)
 
@@ -100,38 +125,38 @@ class Crawler:
                 element = self.driver.find_element(by="id", value="content")
                 text = element.get_attribute('innerText')
             # self.driver.close()
-            logger.debug(f"found text - {link}")
+            self.logger.debug(f"found text - {link}")
             self.add_text(link, text)
 
         except NoSuchElementException:
             # page contains no text
-            logger.debug(f"no text, trying links - {link}")
+            self.logger.debug(f"no text, trying links - {link}")
             self.get_links(link)
 
 def get_file_path(link):
     return "fanfics/" + re.sub(r"[^0-9]+", "", link) + ".txt" 
 
 
+def new_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
 
-logger = logging.getLogger('crawl')
-logger.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
 
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+    # create formatter
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
 
-# create formatter
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-formatter = logging.Formatter('%(asctime)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
 
-# add formatter to ch
-ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
 
-# add ch to logger
-logger.addHandler(ch)
+    return logger
 
 if __name__ == "__main__":
-    logger.debug(f"start")
-    with Crawler() as crawler:
-        crawler.run(3)
-    # ner_to_csv()
+    crawler = Crawler(4, 10000)
+    crawler.run()
