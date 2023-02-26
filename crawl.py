258af3538de30f963780595bb4319d067dc7d821
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-02-26 15:11:08 +0500

    Added logging

--- crawl.py
+++ crawl.py
@@ -7,21 +7,26 @@ import spacy
 import re
 from itertools import zip_longest
 
-nlp = spacy.load("ru_core_news_lg")
-query = "https://ficbook.net/find?fandom_filter=originals&fandom_group_id=1&pages_range=1&pages_min=&pages_max=&transl=1&likes_min=&likes_max=&rewards_min=&date_create_min=2022-01-05&date_create_max=2022-02-05&date_update_min=2022-01-05&date_update_max=2022-02-05&title=&sort=1&rnd=152877722&find=Найти%21&p={}"
+import logging
+
+query = "https://ficbook.net/find?fandom_filter=originals&fandom_group_id=1&pages_range=1&pages_min=&pages_max=&ratings%5B%5D=5&ratings%5B%5D=6&transl=1&likes_min=&likes_max=&rewards_min=&date_create_min=2023-02-25&date_create_max=2023-02-25&date_update_min=2023-02-25&date_update_max=2023-02-25&title=&sort=1&rnd=679840539&find=%D0%9D%D0%B0%D0%B9%D1%82%D0%B8%21&p={}"
 
 
 def get_links(seed):
+    logger.debug(f"starting chrome for\t{seed}")
     driver = uc.Chrome()
+    logger.debug(f"started chrome for\t{seed}")
     with driver:
         # open the page with more links
         driver.get(seed)
+    logger.debug(f"opened page for\t{seed}")
     with driver:
         # find link tags
         elements = driver.find_elements(By.CLASS_NAME, "visit-link")
         # extract links from tags
         links = [i.get_attribute('href') for i in elements]
         links = [link for link in links if link]
+    logger.debug(f"found elements for\t{seed}")
     driver.close()
     driver.quit()
     return links
@@ -32,22 +37,26 @@ def get_file_path(link):
 
 
 def get_text_or_links(link):
+    logger.debug(f"starting chrome for\t{link}")
     driver = uc.Chrome()
+    logger.debug(f"started chrome for\t{link}")
     if isfile(get_file_path(link)):
         return
     with driver:
         # open the page with text
         driver.get(link)
+    logger.debug(f"opened page for\t{link}")
     try:
         with driver:
             element = driver.find_element(by="id", value="content")
             text = element.get_attribute('innerText')
         driver.close()
         driver.quit()
+        logger.debug(f"found text, quit for\t{link}")
         return text
     except NoSuchElementException:
         # page contains no text
-        print(link, "no text found. attempting links")
+        logger.debug(f"no text found, links for \t{link}")
         return get_links(link)
 
 
@@ -59,7 +68,7 @@ def scrape_for_fanfics(pagecount=2):
         links.extend(get_links(seed))
     while links:
         link = links.pop()
-        print(link)
+        logger.debug(f"{link}")
         text = get_text_or_links(link)
         if isinstance(text, list):
             links.extend(text)
@@ -67,6 +76,24 @@ def scrape_for_fanfics(pagecount=2):
             with open(get_file_path(link), "w", encoding="utf8") as file:
                 file.write(text)
 
+
+logger = logging.getLogger('crawl')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
 if __name__ == "__main__":
-    scrape_for_fanfics(2)
+    scrape_for_fanfics(20)
+    logger.debug(f"start")
     # ner_to_csv()
